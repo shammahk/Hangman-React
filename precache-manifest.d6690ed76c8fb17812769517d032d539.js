814fe4c70dad94c55010aa4b2eb0ba82cc75e5c9
shammah <shammahk1@gmail.com>
2020-01-30 18:01:56 +0100

    Updates

---
+++ precache-manifest.d6690ed76c8fb17812769517d032d539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d19df27a2cf0d58f2fd47d0e7ea3b7",
+    "url": "/Hangman-React/index.html"
+  },
+  {
+    "revision": "41b9515f047828c5920b",
+    "url": "/Hangman-React/static/css/main.faf6a24d.chunk.css"
+  },
+  {
+    "revision": "7f946543bb5ae86d96d3",
+    "url": "/Hangman-React/static/js/2.58c5160e.chunk.js"
+  },
+  {
+    "revision": "41b9515f047828c5920b",
+    "url": "/Hangman-React/static/js/main.02b64afe.chunk.js"
+  },
+  {
+    "revision": "8937a08c9c67b867c0a0",
+    "url": "/Hangman-React/static/js/runtime-main.f3a4395d.js"
+  }
+]);
